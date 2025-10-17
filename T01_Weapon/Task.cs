7272f395f0fdeda55fcc40c847981ee34d3c893e
Maybeonce <a-u-rom@yandex.ru>
2025-10-17 14:51:29 +0400

    +

---
+++ T01_Weapon/Task.cs
@@ -0,0 +1,58 @@
+﻿namespace T01_Weapon
+{
+    class Weapon
+    {
+        private int _damage;
+        private int _bullets;
+
+        public void Fire(Player player)
+        {
+            if (_bullets > 0)
+            {
+                player.TryTakeDamage(_damage);
+                _bullets -= 1;
+            }
+        }
+    }
+
+    class Player
+    {
+        private int _health;
+
+        public int Health => _health;
+
+        public bool TryTakeDamage(int damage)
+        {
+            if (damage < 0)
+            {
+                return false;
+            }
+
+            if (_health >= damage)
+            {
+                _health -= damage;
+                return true;
+            }
+            else
+            {
+                _health = 0;
+                return false;
+            }
+        }
+    }
+
+    class Bot
+    {
+        private Weapon _weapon;
+
+        public void Initialize(Weapon weapon)
+        {
+            _weapon = weapon;
+        }
+
+        public void OnSeePlayer(Player player)
+        {
+            _weapon.Fire(player);
+        }
+    }
+}
