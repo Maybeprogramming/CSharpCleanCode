c80e11c6bb5f8350c4e5682dd9817ad2db7595ca
Maybeonce-HomePc <a-u-rom@yandex.ru>
2025-10-21 22:35:41 +0400

    +

---
+++ T02_Test/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using T02_WebMarket;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using NUnit.Framework.Internal;
+using NUnit.Framework.Internal.Commands;
+using NUnit.Framework.Legacy;
+
+namespace T02_Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Cart cart1 = new Cart();
+
+            string str = cart1.Order().Paylink;
+
+            Assert.That(str, Is.Not.Empty);
+        } 
+    }
+}
