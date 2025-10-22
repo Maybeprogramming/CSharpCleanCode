d8337c70511e56479767af99ec9d7be5ac3a70f8
Maybeonce <a-u-rom@yandex.ru>
2025-10-22 11:23:29 +0400

    +

--- T02_WebMarket/Program.cs
+++ T02_WebMarket/Program.cs
@@ -4,15 +4,16 @@
     {
         static void Main(string[] args)
         {
+            #region TEST
             Cart cart1 = new Cart();
             Console.WriteLine($"{cart1.Order().Paylink}");
             Console.ReadKey();
+            #endregion
 
             Good iPhone12 = new Good("IPhone 12");
             Good iPhone11 = new Good("IPhone 11");
 
             Warehouse warehouse = new Warehouse();
-
             Shop shop = new Shop(warehouse);
 
             warehouse.Delive(iPhone12, 10);
@@ -34,53 +35,87 @@
         }
     }
 
-    public class Cart
+    public class Cart : IOrderable
     {
-        private string _store = "https://online-store.ru/Paylink?";
+        private string _storeAdressURL;
 
-        public string Paylink => _store + GetRandomString();
+        private readonly List<Cell> _cells;
+
+        public Cart()
+        {
+            _storeAdressURL = "https://online-store.ru/Paylink?"; ;
+            _cells = new List<Cell>();
+        }
+
+        public string Paylink => CreatePaylink(_storeAdressURL);
 
         public void Add(Good good, int amount)
         {
             Console.WriteLine($"{nameof(Add)}  - Метод не реализован!");
         }
 
-        public Cart Order()
+        public IOrderable Order()
         {
-            Console.WriteLine($"{nameof(Order)}  - Метод не реализован!");
             return this;
         }
 
         public void ShowGoods()
         {
-            Console.WriteLine($"{nameof(ShowGoods)}  - Метод не реализован!");
+            int index = 0;
+
+            Console.WriteLine($"На складе имеются следующие товары:");
+
+            foreach (var cell in _cells)
+            {
+                Console.WriteLine($"{++index}: {cell.Good.Name} - [{cell.Amount}] шт.");
+            }
         }
 
-        private string GetRandomString()
+        private string CreatePaylink(string storeAdressURL)
         {
             Random random = new Random();
-            int randomNumber;
-            int minNumber = 0;
-            int maxNumber = 26;
-            int staticNumber = 65;
-            int stringLenth = 20;
-            string generatedString = "";
+            int randomValue;
+            int minValue = 0;
+            int maxValue = 26;
+            int staticValue = 65;
+            int payStringLenth = 20;
+            string generatedString = String.Empty;
+            char letter;
 
-            for (int i = 0; i < stringLenth; i++)
+            for (int i = 0; i < payStringLenth; i++)
             {
-                randomNumber = random.Next(minNumber, maxNumber);
-                char letter = Convert.ToChar(randomNumber + staticNumber);
+                randomValue = random.Next(minValue, maxValue);
+                letter = Convert.ToChar(randomValue + staticValue);
 
                 generatedString += letter;
             }
 
-            return generatedString;
+            return storeAdressURL + generatedString;
         }
+    }
+
+    public interface IOrderable
+    {
+        public string Paylink { get; }
+    }
+
+    public class Cell
+    {
+        public Cell(Good good, int amount)
+        {
+            Good = good;
+            Amount = amount;
+        }
+
+        public Good Good { get; private set; }
+
+        public int Amount { get; private set; }
     }
 
     public class Shop
     {
         private Warehouse _warehouse;
+        private Cart _cart;
 
         public Shop(Warehouse warehouse)
         {
@@ -89,21 +124,55 @@
 
         public Cart Cart()
         {
-            Console.WriteLine($"{nameof(Cart)}  - Метод не реализован!");
-            return new Cart();
+            _cart = new Cart();
+            return _cart;
         }
     }
 
     public class Warehouse
     {
+        private readonly List<Cell> _cells;
+
+        public Warehouse()
+        {
+            _cells = new();
+        }
+
         public void Delive(Good good, int amount)
         {
-            Console.WriteLine($"{nameof(Delive)} - Метод не реализован!");
+            Cell cell = new Cell(good, amount);
+
+            if (IsCellContainsGood(cell))
+            {
+                MergeCell(cell);
+            }
+            else
+            {
+                _cells.Add(cell);
+            }
+        }
+
+        private void MergeCell(Cell newCell)
+        {
+            Cell cell = _cells.First(cell => cell.Good.Name == newCell.Good.Name);
+            cell = new Cell(cell.Good, cell.Amount + newCell.Amount);
+        }
+
+        private bool IsCellContainsGood(Cell validateCell)
+        {
+            return _cells.Where(cell => cell.Good.Name == validateCell.Good.Name).Count() > 0;
         }
 
         public void ShowAll()
         {
-            Console.WriteLine($"{nameof(ShowAll)}  - Метод не реализован!");
+            int index = 0;
+
+            Console.WriteLine($"На складе имеются следующие товары:");
+
+            foreach (var cell in _cells)
+            {
+                Console.WriteLine($"{++index}: {cell.Good.Name} - [{cell.Amount}] шт.");
+            }
         }
     }
 
