181ee2c8b9c2e523a038445a3e13d747f529ae11
Maybeonce <132255172+Maybeprogramming@users.noreply.github.com>
2025-10-29 14:53:38 +0400

    +

--- T11_MethodsSetter/Program.cs
+++ T11_MethodsSetter/Program.cs
@@ -1,23 +1,22 @@
 ﻿namespace T11_MethodsSetter
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
-        }
+            Console.Title = "ДЗ: 17. Методы set должны устанавливать значение из параметра";
 
-        public static void SetNewObject()
+        public static void CreateObject()
         {
             //Создание объекта на карте
         }
 
-        public static void SetChance()
+        public static void GenerateChance()
         {
             _chance = Random.Range(0, 100);
         }
 
-        public static int SetSalary(int hoursWorked)
+        public static int GetSalary(int hoursWorked)
         {
             return _hourlyRate * hoursWorked;
         }
