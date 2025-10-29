9bfde19315b6c16913fc12a42a479781555a7881
Maybeonce-HomePc <a-u-rom@yandex.ru>
2025-10-29 19:44:25 +0400

    +

--- T11_MethodsSetter/Program.cs
+++ T11_MethodsSetter/Program.cs
@@ -2,21 +2,22 @@
 {
     public class Program
     {
-        static void Main()
+        public static void Main()
         {
             Console.Title = "ДЗ: 17. Методы set должны устанавливать значение из параметра";
+        }
 
         public static void CreateObject()
         {
             //Создание объекта на карте
         }
 
-        public static void GenerateChance()
+        public static void SetRandomChance()
         {
             _chance = Random.Range(0, 100);
         }
 
-        public static int GetSalary(int hoursWorked)
+        public static int CalculateSalary(int hoursWorked)
         {
             return _hourlyRate * hoursWorked;
         }
--- T12_GroupFieldsByPrefix/Program.cs
+++ T12_GroupFieldsByPrefix/Program.cs
@@ -2,35 +2,102 @@
 {
     public class Program
     {
-        static void Main()
+        public static void Main()
         {
             Console.Title = "20. Группировка полей по префиксу";
         }
     }
 
-    class Player
+    public class Player
     {
-        public string Name { get; private set; }
-        public float WeaponCooldown { get; private set; }
-        public float MovementDirectionX { get; private set; }
-        public float MovementDirectionY { get; private set; }
-        public int Age { get; private set; }
-        public int WeaponDamage { get; private set; }
-        public float MovementSpeed { get; private set; }
+        private readonly PlayerInfo _playerInfo;
+        private readonly Weapon _weapon;
+        private readonly Movement _movement;
 
-        public void Move()
+        public Player(PlayerInfo playerInfo, Weapon weapon, Movement movement)
         {
-            //Do move
+            ArgumentNullException.ThrowIfNull(playerInfo, nameof(playerInfo));
+            ArgumentNullException.ThrowIfNull(weapon, nameof(playerInfo));
+            ArgumentNullException.ThrowIfNull(movement, nameof(playerInfo));
+
+            _playerInfo = playerInfo;
+            _weapon = weapon;
+            _movement = movement;
         }
 
         public void Attack()
         {
             //attack
         }
+    }
+
+    public class PlayerInfo
+    {
+        public PlayerInfo(string name, int age)
+        {
+            ArgumentOutOfRangeException.ThrowIfNullOrEmpty(name, nameof(name));
+            ArgumentOutOfRangeException.ThrowIfNegative(age, nameof(age));
+
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; }
+        public int Age { get; }
+    }
+
+    public class Movement
+    {
+        public Movement(float speed)
+        {
+            ArgumentOutOfRangeException.ThrowIfNegative(speed, nameof(speed));
+
+            Speed = speed;
+        }
+
+        public float Speed { get; private set; }
+
+        public void Move(Vector2D targetPosition)
+        {
+            //Do move
+        }
+    }
+
+    public class Weapon
+    {
+        public Weapon(int damage, float cooldown)
+        {
+            ArgumentOutOfRangeException.ThrowIfNegative(damage, nameof(damage));
+            ArgumentOutOfRangeException.ThrowIfNegative(cooldown, nameof(cooldown));
+
+            Damage = damage;
+            Cooldown = cooldown;
+        }
+
+        public int Damage { get; private set; }
+        public float Cooldown { get; private set; }
 
         public bool IsReloading()
         {
             throw new NotImplementedException();
+        }
+    }
+
+    public struct Vector2D
+    {
+        public Vector2D(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public float X { get; private set; }
+        public float Y { get; private set; }
+
+        public void Set(float x, float y) 
+        { 
+            X = x;
+            Y = y;
         }
     }
 }
